4402bb1682353669ac7ad29b6fd19371427a2b58
yangsooplus <yangsooplus@naver.com>
2023-08-31 17:17:40 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+group = "com.yangsooplus"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+    implementation("com.arkivanov.decompose:decompose:2.0.1")
+
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "decompose-navigation"
+            packageVersion = "1.0.0"
+        }
+    }
+}
